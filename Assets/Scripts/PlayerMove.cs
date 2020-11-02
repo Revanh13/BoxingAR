bc6adbcae15a76d4a8f5b3ff101831986bebf6a5
Revanh13 <vana130102@icloud.com>
2020-11-02 14:49:43 +0500

    first commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using easyar;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public FloatingJoystick floatingJoystick;
+    private float speed = 10f;
+
+    public void Update()
+    {
+        Vector3 direction = Vector3.up * floatingJoystick.Vertical + Vector3.left * floatingJoystick.Horizontal;
+        direction.z = transform.position.z;
+        
+        if (floatingJoystick.Horizontal != 0 || floatingJoystick.Vertical != 0)
+		{
+            transform.position = Vector3.Lerp(transform.position, direction, Time.deltaTime * speed);
+		}
+    }
+}
