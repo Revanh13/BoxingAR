8233203616f0d7118310f5751e18b34eb60f10b3
Revanh13 <vana130102@icloud.com>
2020-11-02 19:38:45 +0500

    finally anim

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public FloatingJoystick floatingJoystick;
-    private float speed = 5f;
+    private float speed = 3.5f;
     private Animator animator;
 
 	private void Start()
@@ -20,6 +20,11 @@ public class PlayerMove : MonoBehaviour
         {
             transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - floatingJoystick.Horizontal,
                 transform.position.y + floatingJoystick.Vertical, transform.position.z), Time.deltaTime * speed);
+
+            if (floatingJoystick.Vertical > 0) animator.SetBool("isBack", false);
+            else animator.SetBool("isBack", true);
+            if (floatingJoystick.Horizontal > 0) animator.SetBool("isRight", true);
+            else animator.SetBool("isRight", false);
 
             if (Mathf.Abs(floatingJoystick.Vertical) > Mathf.Abs(floatingJoystick.Horizontal))
             {
