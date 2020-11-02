cf5f827bcb83bef4c9b421f0bd04a781437a937a
Revanh13 <vana130102@icloud.com>
2020-11-02 19:20:51 +0500

    animation

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,16 +6,36 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public FloatingJoystick floatingJoystick;
-    private float speed = 10f;
+    private float speed = 5f;
+    private Animator animator;
 
-    public void Update()
+	private void Start()
+	{
+        animator = transform.GetChild(0).gameObject.GetComponent<Animator>();
+	}
+
+	public void Update()
     {
-        Vector3 direction = Vector3.up * floatingJoystick.Vertical + Vector3.left * floatingJoystick.Horizontal;
-        direction.z = transform.position.z;
-        
         if (floatingJoystick.Horizontal != 0 || floatingJoystick.Vertical != 0)
-		{
-            transform.position = Vector3.Lerp(transform.position, direction, Time.deltaTime * speed);
-		}
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - floatingJoystick.Horizontal,
+                transform.position.y + floatingJoystick.Vertical, transform.position.z), Time.deltaTime * speed);
+
+            if (Mathf.Abs(floatingJoystick.Vertical) > Mathf.Abs(floatingJoystick.Horizontal))
+            {
+                animator.SetFloat("SpeedWalk", Mathf.Abs(floatingJoystick.Vertical));
+                animator.SetBool("isSide", false);
+            }
+            else
+            {
+                animator.SetFloat("SpeedSideWalk", Mathf.Abs(floatingJoystick.Horizontal));
+                animator.SetBool("isSide", true);
+            }
+        }
+        else
+        {
+            animator.SetFloat("SpeedWalk", 0);
+            animator.SetFloat("SpeedSideWalk", 0);
+        }
     }
 }
